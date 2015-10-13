5bb5c4941051d1b2ebb14ab2529c8022dc24432d
sergey <chernushov88@mail.ru>
2015-10-13 07:33:27 +0300

    responsive index.html 320px

--- js/myscript.js
+++ js/myscript.js
@@ -1,7 +1,39 @@
 ﻿/*-----------------------------------------------------------------------------------*/
-/*	Slider
+/*	responsive menu
 /*-----------------------------------------------------------------------------------*/
-
+$(document).ready(function(){
+		var touch = $('#touch-menu');
+	    var menu = $('.nav-header-left');
+	 
+	    $(touch).on('click', function(e) {
+	        e.preventDefault();
+	        menu.slideToggle();
+	    });
+	    $(window).resize(function(){
+	        var wid = $(window).width();
+	        if(wid > 768 && menu.is(':hidden')) {
+	            menu.removeAttr('style');
+	        }
+	    });
+	});
+/*-----------------------------------------------------------------------------------*/
+/*	responsive catalog
+/*-----------------------------------------------------------------------------------*/
+$(document).ready(function(){
+		var touch = $('#touch-catalog');
+	    var menu = $('.mobile-catalog');
+	 
+	    $(touch).on('click', function(e) {
+	        e.preventDefault();
+	        menu.slideToggle();
+	    });
+	    $(window).resize(function(){
+	        var wid = $(window).width();
+	        if(wid > 768 && menu.is(':hidden')) {
+	            menu.removeAttr('style');
+	        }
+	    });
+	});
 /*-----------------------------------------------------------------------------------*/
 /*  PopUp
 /*-----------------------------------------------------------------------------------*/
